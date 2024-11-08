d52d99ebd669ed37b3ac89ced09a4cb3708f1d44
LiamCHutton <Liam_Hutton@insideranken.org>
2024-11-08 09:46:55 -0600

    Update

---
+++ labs/JS_CH06_LAB/faqs/faqs.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// the event handler for the click event of each h2 element
+const toggle = evt => {
+    const aElement = evt.currentPage;                    // get the clicked a element
+    const h2Element = aElement.parentElement;              // get the clicked h2 element
+    const divElement = h2Element.nextElementSibling;     // get h2's sibling div
+
+    //h2Element.classList.toggle("minus");
+    //divElement.classList.toggle("open");
+    if (h2Element.hasAttribute("class")) {
+        h2Element.removeAttribute("class")
+    }
+    else {
+        h2Element.className = "minus";
+    }
+
+    if (divElement.hasAttribute("class")) {
+        divElement.removeAttribute("class")
+    }
+    else {
+        divElement.className = "open";
+    }
+
+    evt.preventDefault();   // cancel default action of h2 tag's <a> tag
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // get the h2 tags
+    const aElements = faqs.querySelectorAll("#faqs h2 a");
+    
+    // attach event handler for each h2 tag	    
+    for (let aElement of aElements) {
+        aElement.addEventListener("click", toggle);
+    }
+    // set focus on first h2 tag's <a> tag
+    //aElements[0].firstChild.focus();     
+    aElements.focus();  
+});
